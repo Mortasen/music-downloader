734cf18e9eb44b5e14948494961761c566733025
Namerif <namerif7@gmail.com>
2019-11-22 20:27:37 +0200

    added dict_without_keys func

--- utils.py
+++ utils.py
@@ -72,3 +72,8 @@ def format_time (seconds):
     return duration
 
 
+
+def dict_without_keys(dict_, *keys):
+    # {'a': 5, 'b': 6, 'c': 7} ('c') >> {'a': 5, 'b': 6}
+    return {k:dict_[k] for k in dict_ if not k in keys}
+
