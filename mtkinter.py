ef1f42d5743b8a6c95e45944ac58887dc9c507a1
Namerif <namerif7@gmail.com>
2019-11-27 21:20:50 +0200

    added EntryFileLocation and EntryDirectory

--- mtkinter.py
+++ mtkinter.py
@@ -1,8 +1,19 @@
 from tkinter import *
-from tkinter.ttk import *
-from tkinter.filedialog import *
-from tkinter.messagebox import *
-from tkinter.font import *
+from tkinter.ttk import Progressbar, Spinbox, Combobox
+from tkinter import filedialog as tkfd
+from tkinter import messagebox as tkmb
+from tkinter import font as tkfont
+
+CONFLICTS = ['Button', 'Checkbutton',
+             'Entry', 'Frame',
+             'Label', 'LabelFrame',
+             'Menubutton', 'OptionMenu',
+             'PanedWindow', 'Radiobutton',
+             'Scale', 'Scrollbar',
+             'Spinbox', 'Widget',
+             '_flatten', '_join',
+             '_splitdict', '_stringify']
+
 
 class EntryWithPlaceholder:
 
@@ -52,5 +63,54 @@ class EntryWithPlaceholder:
             ___user_focus_in_func = func
         elif event == '<FocusOut>':
             ___user_focus_out_func = func
+        else:
+            self.___obj.bind(event, func)
+
+
+class EntryFileLocation:
+
+    def __init__ (self, root, **options):
+        self.___obj = Entry(root, **options)
+        self.___obj.bind('<Button-1>', self.___click)
+        self.__dir__ = self.___obj.__dir__
+        self.___user_button1_func = lambda *_: None
+
+    def __getattr__ (self, name):
+        return getattr(self.___obj, name)
+
+    def ___click (self, *args):
+        file_path = tkfd.askopenfilename()
+        self.___obj.delete(0, 'end')
+        self.___obj.insert(0, file_path)
+        self.___user_button1_func(*args)
+
+    def bind (self, event, func):
+        if event == '<Button-1>':
+            ___user_button1_func = func
+        else:
+            self.___obj.bind(event, func)
+
+
+
+class EntryDirectory:
+
+    def __init__ (self, root, **options):
+        self.___obj = Entry(root, **options)
+        self.___obj.bind('<Button-1>', self.___click)
+        self.__dir__ = self.___obj.__dir__
+        self.___user_button1_func = lambda *_: None
+
+    def __getattr__ (self, name):
+        return getattr(self.___obj, name)
+
+    def ___click (self, *args):
+        directory = tkfd.askdirectory()
+        self.___obj.delete(0, 'end')
+        self.___obj.insert(0, directory)
+        self.___user_button1_func(*args)
+
+    def bind (self, event, func):
+        if event == '<Button-1>':
+            ___user_button1_func = func
         else:
             self.___obj.bind(event, func)
